e8ad5b1b40eacf25e0e26a1235af1a14cb156f4f
Audun Kvasbø <audun@kvasbo.no>
2017-12-29 20:47:30 +0100

    Intern bildevisning og deling av bilder

--- Glimmer/src/Components/UXElements/GlimmerImage.js
+++ Glimmer/src/Components/UXElements/GlimmerImage.js
@@ -1,21 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Image, TouchableOpacity, Linking, View } from 'react-native';
+import { Image, TouchableOpacity, Linking, View, ActionSheetIOS } from 'react-native';
+import ImageViewer from 'ImageViewer';
 
 export default class GlimmerImage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showing: false };
+    this.state = {
+      shown: false,
+      curIndex: 0
+    };
   }
 
+  imgsArr = [
+    this.props.uri,
+  ];
+
+  closeViewer() {
+    this.setState({
+      shown: false,
+      curIndex: 0,
+    });
+  }
 
   render() {
     return (
       <View>
         <TouchableOpacity
           onPress={() => {
-            this.setState({ showing: true });
-            Linking.openURL(this.props.uri);
+            this.setState({ shown: true });
+            //Linking.openURL(this.props.uri);
+          }}
+          onLongPress={() => {
+            const options = { url: this.props.uri }
+            ActionSheetIOS.showShareActionSheetWithOptions(options, (error) => { console.log(error); }, (success, method) => { console.log(success, method); } );
           }}
         >
           <Image
@@ -24,6 +42,12 @@ export default class GlimmerImage extends React.Component {
             style={{ height: 240, width: '100%', marginBottom: 10, marginTop: 10 }}
           />
         </TouchableOpacity>
+        <ImageViewer
+          shown={this.state.shown}
+          imageUrls={this.imgsArr}
+          onClose={this.closeViewer.bind(this)}
+          index={this.state.curIndex} 
+        />
       </View>
     );
   }
